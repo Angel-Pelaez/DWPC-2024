bac5f88c8cd6f837c50a6d9bbe98b486f559d95b
Angel-Pelaez <160808786+Angel-Pelaez@users.noreply.github.com>
2024-02-22 05:00:26 +0000

    🏆Feature: Babel en el Front-end

---
+++ webpack.dev.config.js
@@ -0,0 +1,66 @@
+// Notas importanes
+// El archivo de configuración debe usar ES5
+
+// Importar un administrador de rutas de archivos
+const path = require('path');
+
+// Exportamos un Configuration Options Object
+module.exports = {
+  // 0. Estableciendo el modo produccion
+  mode: "development",  
+  // 1. Estableciendo el archivo indexador
+  // del front-end
+  entry: "./client/index.js",
+  // 2. Estableciendo el archivo de salida
+  output: {
+    // 2.1 Ruta Absoluta de Salida
+    path: path.resolve(__dirname, "public"),
+    // 2.2 Nombre del archivo de salida
+    filename: "bundle.js",
+    // 2.3 Ruta base de archivos estaticos
+    publicPath: "/"
+  },
+  // 3. Servidor de desarrollo
+  devServer: {
+    // 3.1 Folder de estaticos
+    static: path.join(__dirname, 'public'),
+    // 3.2 Puerto del servidor de desarrollo
+    port: 8080,
+    // 3.3 Definiendo el HOST
+    host: '0.0.0.0'
+  },
+    // 4. Configuración de los loaders
+    module: {
+      rules: [
+        // 3.1 Reglas para archivos JS
+        {
+          // 3.1.1 Expresión regular para identificar archivos
+          test: /\.js$/,
+          // 3.1.2 Excluir archivos de la carpeta node_modules
+          exclude: /node_modules/,
+          // 3.1.3 Usar el loader de babel
+          use:[
+            {
+              loader: "babel-loader",
+              // 3.1.4 Opciones de configuración de babel
+              options: {
+                presets: [
+                  [
+                    '@babel/preset-env',
+                    // 3.1.5 Opciones de configuración de present-env
+                    {
+                      "modules": false,
+                      "useBuiltIns": "usage",
+                      // 3.1.6 Corejs para usar con polyfills
+                      "targets": '> 0.25%, not dead',
+                      "corejs": 3
+                    }
+                  ]
+                ]
+              }
+            }
+          ]
+        }
+      ]   
+    }
+};
